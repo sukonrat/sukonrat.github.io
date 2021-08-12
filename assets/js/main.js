30967cf5087c082ca1ae360dfdec3132cc293b38
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-08-12 19:38:04 +1000

    created qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,23 @@
   skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
   })
+
+    /* =========== QUALIFICATION TABS ============ */
+    const tabs = document.querySelectorAll('[data-target]'),
+          tabContents = document.querySelectorAll('[data-content]')
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      const target = document.querySelector(tab.dataset.target);
+
+      tabContents.forEach(tabContent => {
+          tabContent.classList.remove('qualification-active')
+      })
+      target.classList.add('qualification-active')
+
+      tab.forEach(tab => {
+        tab.classList.remove('qualification-active')
+      })
+      tab.classList.add('qualification-active')
+    })
+  })
