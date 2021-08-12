6082a03c93907f6d83a266cb84b59e39cf147966
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-08-12 21:15:27 +1000

    created services

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,26 @@
       tab.classList.add('qualification-active')
     })
   })
+
+      /* =========== SERVICES MODAL ============ */
+const modalViews = document.querySelectorAll('.services-modal'),
+      modalBtns = document.querySelectorAll('.services-button'),
+      modalCloses = document.querySelectorAll('.services-modal-close')
+
+let modal = function (modalClick){
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click',()=>{
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () =>{
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
