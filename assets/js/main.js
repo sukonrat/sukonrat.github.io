648a4b58d81a4501acbcc13821851eef5bdad28f
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-08-13 14:33:38 +1000

    created portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,3 +92,17 @@ modalCloses.forEach((modalClose) => {
     })
   })
 })
+
+      /* =========== PORTFOLIO SWIPER ============ */
+      let swiper = new Swiper(".portfolio-container", {
+        cssMode: true,
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
